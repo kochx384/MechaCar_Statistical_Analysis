71deef883c32a694471fd35ba511f443284bf30d
kochx384 <kochx384@gmail.com>
2021-10-17 17:50:14 -0500

    add suspension coils summary

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,4 +2,14 @@ library(dplyr)
 
 mecha_car_data <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
+lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, mecha_car_data)
+
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, mecha_car_data))
+
+suspension_coil <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+
+total_summary <- summarize(suspension_coil, Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+lot_summary <- suspension_coil %>% 
+  group_by(Manufacturing_Lot) %>%
+  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
