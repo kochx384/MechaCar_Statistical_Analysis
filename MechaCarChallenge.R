080954b407293512725ead61188da39cfb88888b
kochx384 <kochx384@gmail.com>
2021-10-17 19:32:43 -0500

    add ttests to rscript

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,15 @@ total_summary <- summarize(suspension_coil, Mean = mean(PSI), Median = median(PS
 lot_summary <- suspension_coil %>% 
   group_by(Manufacturing_Lot) %>%
   summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+t.test(suspension_coil$PSI, mu=1500)
+
+Lot1 <- subset(suspension_coil, Manufacturing_Lot == "Lot1")
+Lot2 <- subset(suspension_coil, Manufacturing_Lot == "Lot2")
+Lot3 <- subset(suspension_coil, Manufacturing_Lot == "Lot3")
+
+t.test(Lot1$PSI, mu=1500)
+t.test(Lot2$PSI, mu=1500)
+t.test(Lot3$PSI, mu=1500)
+  
+  
